0a5c5ccdc4147aabc0d028fd5b8bd95a9033dbae
perchopick <ivanhuay@gmail.com>
2017-04-17 10:01:08 -0300

    fix for numeric identifier

--- makedictionary.py
+++ makedictionary.py
@@ -7,7 +7,7 @@ import itertools
 class DictionaryMaker:
 	def __init__(self):
 		self.aditionalData = []
-		self.simpleCollection = ["admin","adm","adm","2015","2016","2017","2014","2013"]
+		self.simpleCollection = ["admin","adm","adm","2015","2016","2017","2014","2013",".","-","_","@"]
 		self.convinationLevel = 2
 		self.domainName = False
 		self.fullName = False
@@ -62,11 +62,19 @@ class DictionaryMaker:
 	def processDate(self,inStr):
 		response = []
 		if "/" in inStr:
-			response.extends(inStr.split("/"))
+			response.extend(inStr.split("/"))
 		if "-" in inStr:
-			response.extends(inStr.split("-"))
+			response.extend(inStr.split("-"))
 		return response
-
+	def processIdentification(self,inStr):
+                numbers = [str(s) for s in inStr.split() if s.isdigit()]
+                response = []
+                for number in numbers:
+                        for i in range(1, len(number)):
+				response.append(number[0:i])
+				response.append(number[:i])
+		response = self.cleanList(response)
+                return response
 	def makeQuestion(self,questionStr,storeStr):
 		nextQuestion = True
 
@@ -83,38 +91,44 @@ class DictionaryMaker:
 				nextQuestion = False
 		setattr(self,storeStr,storeSelf)
 	def processInput(self):
-		print("processing data...")
+		print("Starting processing...")
         	if len(self.domainName) > 0:
+			print("processing domain name...")
 			for domain in self.domainName:
             			self.simpleCollection.extend(self.processDomain(domain))
 		if len(self.fullName) > 0:
+			print("processing full name...")
 			for fullName in self.fullName:
 				self.simpleCollection.extend(self.processStr(fullName))
         	if len(self.address) > 0:
+			print("processing address...")
 			for address in self.address:
             			self.simpleCollection.extend(self.processAddress(address))
         	if len(self.aditionalData) > 0:
+			print("processing additional data...")
             		for data in self.aditionalData:
                 		self.simpleCollection.extend(self.processStr(data))
 		if len(self.importantDate) > 0:
+			print("processing dates...")
 			for date in self.importantDate:
 				self.simpleCollection.extend(self.processDate(date))
 		if len(self.identification) > 0:
+			print("processing identification...")
 			for identification in self.identification:
-				self.simpleCollection.extend(self.processAddress(identification))
+				self.simpleCollection.extend(self.processIdentification(identification))
 		tempTitles = []
 		for text in self.simpleCollection:
-			if str(text).title() in self.simpleCollection:
+			if not str(text).title() in tempTitles:
 				tempTitles.append(str(text).title())
 		self.simpleCollection.extend(tempTitles)
 
-		cleanList = []
-		for stringPosible in self.simpleCollection:
-			if stringPosible not in cleanList:
-				cleanList.append(stringPosible)
-		self.simpleCollection = cleanList
         	self.greenPrint("Done")
-
+	def cleanList(self,list):
+		cleanList = []
+		for strItem in list:
+			if strItem.strip() not in cleanList:
+				cleanList.append(strItem)
+		return cleanList
 	def greenPrint(self, text):
 		print '\033[92m' + text + " " + u'\u2713' + '\033[0m'
 
@@ -127,9 +141,10 @@ class DictionaryMaker:
 			for j in res:
 				posiblePass = ''.join(j)
 				lines.append(posiblePass)
+
+		lines = self.cleanList(lines)
 		self.makeFile(lines)
 		self.greenPrint("write file done")
-		self.makeFile(lines)
 
 	def makeFile(self, lines):
 		with open('pass.txt', 'a') as passFile:
