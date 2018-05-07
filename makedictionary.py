88ac6f062d3f2af56574e151ddd1e5cc3551b874
perchopick <ivanhuay@gmail.com>
2018-05-07 08:19:19 -0300

    default convination level

--- makedictionary.py
+++ makedictionary.py
@@ -4,47 +4,57 @@ import os
 import itertools
 import operator
 
+
 class DictionaryMaker:
     def __init__(self):
         self.aditionalData = []
-        self.simpleCollection=["admin", "adm", "adm", "2015", "2016", "2017", "2014", "2013", ".", "-", "_", "@"]
+        self.simpleCollection = ["admin", "adm", "adm", "2015",
+                                 "2018", "2016", "2017", "2014", "2013", ".", "-", "_", "@"]
         self.convinationLevel = 2
         self.domainName = False
         self.fullName = False
         self.address = False
         self.importantDate = False
         self.identification = False
+
     def welcome(self):
         print("Welcome human, Please answer the following questions...")
-        self.getInput()
+        self.getInputs()
         self.processInput()
         self.generateDictionary()
-    def getInput(self):
-        print("It is possible to enter an empty response...")
-        self.convinationLevel = int(raw_input("convination level:"))
-        self.makeQuestion("domain name?","domainName")
-        self.makeQuestion("address?","address")
-        self.makeQuestion("full name?","fullName")
-        self.makeQuestion("birthdate or important date?(dd-mm-yyyy)","importantDate")
-        self.makeQuestion("identifier or identification number?", "identification")
-        self.makeQuestion("aditional data?","aditionalData")
 
-    def processNumbers(self,inStr):
+    def getInputs(self):
+        print("Usage: it is possible to enter an empty response...")
+        convination = raw_input("convination level: (2 recomended)")
+        self.convinationLevel = 2
+        if convination != "" :
+            self.convinationLevel = int(convination)
+        self.makeQuestion("domain name?", "domainName")
+        self.makeQuestion("address?", "address")
+        self.makeQuestion("full name?", "fullName")
+        self.makeQuestion(
+            "birthdate or important date?(dd-mm-yyyy)", "importantDate")
+        self.makeQuestion(
+            "identifier or identification number?", "identification")
+        self.makeQuestion("aditional data?", "aditionalData")
+
+    def processNumbers(self, inStr):
         numbers = [str(s) for s in inStr.split() if s.isdigit()]
         response = []
         for number in numbers:
             for i in range(1, len(number)):
-                res = itertools.product(number,repeat = i)
+                res = itertools.product(number, repeat=i)
                 for convination in res:
                     response.append(''.join(convination))
         return response
 
-    def processStr(self,inStr):
+    def processStr(self, inStr):
         response = [str(s) for s in inStr.split() if not s.isdigit()]
         response.append(inStr)
         response.append("".join(inStr.split()))
         return response
-    def processName(self,inStr):
+
+    def processName(self, inStr):
         response = self.processStr(inStr)
         words = [str(s) for s in inStr.split() if not s.isdigit()]
         for word in words:
@@ -54,12 +64,14 @@ class DictionaryMaker:
         for convination in res:
             response.append(''.join(convination))
         return self.cleanList(response)
-    def processDomain(self,inStr):
+
+    def processDomain(self, inStr):
         response = []
         response.append(inStr)
         response.extend(inStr.split("."))
         return response
-    def processAddress(self,inStr):
+
+    def processAddress(self, inStr):
         response = []
         response.append(inStr)
         response.extend(inStr.split())
@@ -67,7 +79,8 @@ class DictionaryMaker:
         response.extend(self.processNumbers(inStr))
         response.extend(self.processStr(inStr))
         return response
-    def processDate(self,inStr):
+
+    def processDate(self, inStr):
         response = []
         if "/" in inStr:
             response.extend(inStr.split("/"))
@@ -85,11 +98,12 @@ class DictionaryMaker:
         res = itertools.combinations(response, 3)
         for convination in res:
             response.append(''.join(convination))
-            #response.append('-'.join(convination))
-            #response.append('/'.join(convination))
+            # response.append('-'.join(convination))
+            # response.append('/'.join(convination))
         print("date convinations: " + str(len(response)) + ".")
         return self.cleanList(response)
-    def processIdentification(self,inStr):
+
+    def processIdentification(self, inStr):
         numbers = [str(s) for s in inStr.split() if s.isdigit()]
         response = []
         for number in numbers:
@@ -97,13 +111,14 @@ class DictionaryMaker:
                 response.append(number[0:i])
                 response.append(number[:i])
                 return self.cleanList(response)
-    def makeQuestion(self,questionStr,storeStr):
+
+    def makeQuestion(self, questionStr, storeStr):
         nextQuestion = True
 
-        if not getattr(self,storeStr):
-            setattr(self,storeStr, [])
+        if not getattr(self, storeStr):
+            setattr(self, storeStr, [])
 
-        storeSelf = getattr(self,storeStr)
+        storeSelf = getattr(self, storeStr)
 
         while nextQuestion:
             tempAnswer = raw_input(questionStr + " (empty = next question): ")
@@ -111,7 +126,8 @@ class DictionaryMaker:
                 storeSelf.append(tempAnswer)
             else:
                 nextQuestion = False
-        setattr(self,storeStr,storeSelf)
+        setattr(self, storeStr, storeSelf)
+
     def processInput(self):
         print("Starting processing...")
         if len(self.domainName) > 0:
@@ -137,7 +153,8 @@ class DictionaryMaker:
         if len(self.identification) > 0:
             print("processing identification...")
             for identification in self.identification:
-                self.simpleCollection.extend(self.processIdentification(identification))
+                self.simpleCollection.extend(
+                    self.processIdentification(identification))
         tempTitles = []
         for text in self.simpleCollection:
             if not str(text).title() in tempTitles:
@@ -145,8 +162,10 @@ class DictionaryMaker:
         self.simpleCollection.extend(tempTitles)
 
         self.greenPrint("Done")
-    def cleanList(self,list):
+
+    def cleanList(self, list):
         return sorted(set(list))
+
     def greenPrint(self, text):
         print '\033[92m' + text + " " + u'\u2713' + '\033[0m'
 
@@ -156,24 +175,25 @@ class DictionaryMaker:
         lines = []
         for i in range(1, self.convinationLevel + 1):
             print "starting level: " + str(i) + "."
-            res = itertools.product(self.cleanList(self.simpleCollection), repeat=i)
+            res = itertools.product(self.cleanList(
+                self.simpleCollection), repeat=i)
             for j in res:
                 posiblePass = ''.join(j)
                 lines.append(posiblePass)
-            self.greenPrint( "leven " + str(i) + ": done")
-        print("cleaning List... "+str(len(lines)))
+            self.greenPrint("leven " + str(i) + ": done")
+        print("cleaning List... " + str(len(lines)))
         lines = self.cleanList(lines)
         self.greenPrint("clen list done lines: " + str(len(lines)) + ".")
 
-        print "writing "+str(len(lines))+" lines in file..."
+        print "writing " + str(len(lines)) + " lines in file..."
         self.makeFile(lines)
         self.greenPrint("write file done")
 
     def makeFile(self, lines):
         with open('pass.txt', 'a') as passFile:
             for line in lines:
-                passFile.write(line+'\n')
+                passFile.write(line + '\n')
 
 
-dictionaryMaker  = DictionaryMaker()
+dictionaryMaker = DictionaryMaker()
 dictionaryMaker.welcome()
